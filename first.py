e49ab7af1648390873eb4c7c0b0482369d3d77ef
Aaryan Thakur <princeaaryan06@gmail.com>
2025-05-21 23:10:29 +0545

    working on query parameter  by making new api endpoints

--- first.py
+++ first.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
+import logging
  
 
 app=FastAPI()
-
 
 def load_json():
     with open("patent.json", "r") as f:
@@ -34,3 +34,26 @@ def view_patient(patent_id: str=Path(..., title="The ID of the patent to view", 
         return data[patent_id]
     else:
         raise HTTPException(status_code=404, detail="patent not found")
+    
+
+@app.get("/sort")
+def sort_patent(sort_by:str=Query(..., description="sort on the basis of weight and height"),
+                order:str=Query("asc",description="sort in ascending or descending order")):
+    logging.info(f"sort_by: {sort_by}")
+    
+    valid_fields=["weight", "height"]
+    logging.info(f"sort_by: {sort_by}")
+    if sort_by not in valid_fields:
+        logging.error(f"invalid field")
+        raise HTTPException(status_code=400, detail=f"Invalid  fields select from {valid_fields}")
+    
+    if not  order in ["asc", "desc"]:
+        raise HTTPException(status_code=400, detail="invalid order, select between asc and desc")
+    
+    logging.info(f"order: {order}")
+    data=load_json()
+    sort_order=True if order=="asc" else False
+    sorted_data=sorted(data.values(), key=lambda x:x.get(sort_by,0), reverse=sort_order)
+
+    return sorted_data
+   
